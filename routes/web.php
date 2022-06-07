d433bae4bee89acc0adc31ea0f7d794d9be21de9
noemipintaldi <noemipintaldi@gmail.com>
2022-06-07 19:55:28 +0200

    esercizio completato

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('guest.home');
+//});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::middleware('auth')->name('admin.')
+->prefix('admin')
+->namespace('Admin')
+->group(function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
+});
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where("any", ".*");
